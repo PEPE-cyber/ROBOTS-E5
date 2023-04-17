364e3358d5d5b1ebccaacfea2ba9fafdbe16687e
Jose Varela <a01620480@tec.mx>
2023-04-17 13:56:10 -0600

    reto1

--- build/equipo5/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emanuel/workspaces/Puzzle_bot_ws/src/equipo5
-# Build directory: /home/emanuel/workspaces/Puzzle_bot_ws/build/equipo5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
