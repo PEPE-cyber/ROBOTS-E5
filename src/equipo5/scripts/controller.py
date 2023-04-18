0b687878a9ab0446357f225e811511c230dda372
STARTAPPS-JOSE <114040333+STARTAPPS-JOSE@users.noreply.github.com>
2023-04-18 12:24:01 -0600

    init commit

---
+++ src/equipo5/scripts/controller.py
@@ -0,0 +1,110 @@
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Point
+from math import cos, sin
+
+# ceate a PID controller class
+class PIDController:
+    def __init__(self, kp, ki, kd, dt):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.dt = dt 
+        self.error = 0
+        self.integral = 0
+        self.derivative = 0
+        self.last_error = 0
+        self.setpoint = 0
+
+    def update(self, error):
+        self.error = error
+        self.integral += (self.error + self.last_error) * self.dt / 2
+        self.derivative = (self.error - self.last_error) / self.dt
+        self.last_error = self.error
+
+    def get_control(self):
+        return self.kp * self.error + self.ki * self.integral + self.kd * self.derivative
+    
+    def update_setpoint(self, setpoint):
+        self.setpoint = setpoint
+
+
+setpoint = Point()
+wl = 0.0
+wr = 0.0
+
+def setpoint_callback(msg):
+    global setpoint
+    setpoint = msg
+
+def wl_callback(msg):
+    global wl
+    wl = msg.data
+
+def wr_callback(msg):
+    global wr
+    wr = msg.data
+
+
+# Define constants
+r = 0.05
+L = 0.19
+dt = 0.1
+
+
+# subscribe to the topic /path_generator
+rospy.Subscriber('/set_point', Point, setpoint_callback)
+
+
+# Create controllers for linear and angular velocity
+controller_vel = PIDController(0.1, 0.1, 0.1, dt)
+controller_theta = PIDController(0.1, 0.1, 0.1, dt)
+
+rospy.Subscriber('/pub_wl', Float32, wl_callback)
+rospy.Subscriber('/pub_wr', Float32, wr_callback)
+
+rospy.init_node('pid_controller')
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+twist = Twist()
+
+
+
+angle = 0
+x = 0
+y = 0
+if __name__ == '__main__':
+    try:
+        while not rospy.is_shutdown():
+            # Get current velocities of each wheel
+            v_l = wl
+            v_r = wr
+            # Get current position of the robot from each wheel velocity
+            angle += r * (v_r - v_l) * dt / L
+            x += r * (v_r + v_l) / 2 * dt * cos(angle)
+            y += r * (v_r + v_l) / 2 * sin(angle)
+            
+            # Calculate the error between the current position and the setpoint
+            error = Point()
+            error.x = setpoint.x - x
+            error.y = setpoint.y - y
+            error.z = setpoint.z - angle
+
+            #  Obtain the distance to the setpoint
+            distance = (error.x**2 + error.y**2)**0.5
+            controller_vel.update(distance)
+            controller_theta.update(error.z)
+
+            # Calculate the linear and angular velocity
+            v = controller_vel.get_control()
+            w = controller_theta.get_control()
+
+            # Publish the linear and angular velocity
+            twist.linear.x = v
+            twist.angular.z = w
+            pub.publish(twist)
+            rospy.sleep(dt)
+          
+    except rospy.ROSInterruptException:
+        pass
--- src/equipo5/scripts/path_generator.py
+++ src/equipo5/scripts/path_generator.py
@@ -1,70 +1,34 @@
 #!/usr/bin/env python
  
 import rospy
-from geometry_msgs.msg import Twist
- 
-def move(pub, distance, speed, cmd_vel):
-    
-    cmd_vel.linear.x = speed
-    distance_moved = 0
-    t0 = 0
-    while distance_moved < distance:
-        pub.publish(cmd_vel)
-        if t0 == 0:
-            t0 = t1 = rospy.Time.now().to_sec()
-        else:
-            t1 = rospy.Time.now().to_sec()
-        distance_moved = speed * 1.14 * (t1 - t0)
-        rospy.sleep(0.01)
- 
-    cmd_vel.linear.x = 0
-    pub.publish(cmd_vel)
- 
-    rospy.sleep(0.5)
- 
-def turn(angle, angular_speed, cmd_vel):
-    # Girar a la izquierda
-    cmd_vel.angular.z = angular_speed 
-    t0 = 0
-    angle_turned = 0
-    while angle_turned < angle:
-        pub.publish(cmd_vel)
-        if t0 == 0:
-            t0 = t1 = rospy.Time.now().to_sec()
-        else:
-            t1 = rospy.Time.now().to_sec()
-        angle_turned = angular_speed  * 0.88 * (t1 - t0)
-        rospy.sleep(0.01)
- 
-    cmd_vel.angular.z = 0
-    print(angle_turned)
-    pub.publish(cmd_vel)
-    rospy.sleep(0.5)
+from geometry_msgs.msg import Point
  
 # Inicializar el nodo
 rospy.init_node('path_generator')
 # Crear un publicador para enviar mensajes Twist
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+pub = rospy.Publisher('/set_point', Point, queue_size=10)
  
-# Crear un objeto Twist para enviar los comandos
-cmd_vel = Twist()
+# Crear un objeto Point
+point = Point()
+
+point.linear.x = 0
+point.angular.y = 0
  
-cmd_vel.linear.x = 0
-cmd_vel.angular.z = 0
- 
-pub.publish(cmd_vel)
+pub.publish(point)
 rospy.sleep(2)
 
 if __name__=='__main__':
     try:
-      
-        for i in range(0, 4):
-            move(pub, 2, 0.3, cmd_vel)
-            turn(1.5708, 3, cmd_vel)
-        
-        cmd_vel.linear.x = 0
-        cmd_vel.angular.z = 0
-        pub.publish(cmd_vel)
- 
+        while not rospy.is_shutdown():
+            point.linear.x = 2
+            point.angular.y = 2
+            point.linear.z = 0
+            pub.publish(point)
+            rospy.sleep(2)
+
+        point.linear.x = 0
+        point.angular.y = 0
+        pub.publish(point)
+    
     except rospy.ROSInterruptException:
         pass
